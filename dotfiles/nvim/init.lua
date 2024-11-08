ebc3094a7e372ee7bd8b36eb1b8e6f75678df4f4
FriendlyStepBro <logicye@proton.me>
2024-11-08 10:03:46 +1000

    Added current standard dotfile for arch to the repository

---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,18 @@
+require("vim-config")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", --latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
+local opts = {}
+require("lazy").setup(plugins, opts)
+
+require("mouse-functions")
---
+++ dotfiles/nvim/lua/vim-config.lua
@@ -0,0 +1,37 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=3")
+vim.cmd("set softtabstop=3")
+vim.cmd("set shiftwidth=3")
+vim.cmd("set scrolloff=999")
+vim.cmd("set mouse=")
+-- vim.cmd("set wildmenu=true")
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.keymap.set('n', '<Space>', '<NOP>', { noremap = true, silent = true })
+vim.keymap.set('v', '<Space>', '<NOP>', { noremap = true, silent = true })
+vim.g.mapleader = " "
+
+-- additional keybinds
+vim.keymap.set('n', '<leader>fq', ":qa!<CR>", {})
+vim.keymap.set('n', '<leader>wq', ":wa<CR>:qa<CR>:", {})
+
+vim.keymap.set('n', '<leader>h', '^', { noremap = false, silent = true })
+vim.keymap.set('n', '<leader>l', '$', { noremap = false, silent = true })
+vim.keymap.set('v', '<leader>h', '^', { noremap = false, silent = true })
+vim.keymap.set('v', '<leader>l', '$', { noremap = false, silent = true })
+
+-- insert line about without entering insert mode
+vim.keymap.set('n', '<leader>o', 'm`o<Esc>``', { noremap = false, silent = true })
+vim.keymap.set('n', '<leader>O', 'm`O<Esc>``', { noremap = false, silent = true })
+
+vim.keymap.set('n', '<leader>p', '"+p', { noremap = false, silent = true})
+vim.keymap.set('n', '<leader>y', '"+yy', { noremap = false, silent = true})
+vim.keymap.set('v', '<leader>y', '"+y', { noremap = false, silent = true})
+
+--vim.keymap.set('v', '<leader>j', ':move v:count+1<CR>gv', { noremap = true, silent = true })
+--vim.keymap.set('v', '<leader>k', ':move .-2<CR>gv', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>j', ':move +1<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>k', ':move -2<CR>', { noremap = true, silent = true })
